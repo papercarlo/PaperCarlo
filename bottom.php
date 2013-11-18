84d8e503fb5719b75aa8258ca4264161b76ea424
papercarlo <papercarlo@gmail.com>
2013-11-19 06:26:08 +0800

    test

---
+++ bottom.php
@@ -0,0 +1,11 @@
+<textarea cols="95" rows="5">
+<?php
+include('vadmin/db.php');
+$result = mysql_query("SELECT * FROM tags ORDER BY tag_id ASC");
+while($row = mysql_fetch_assoc($result)){
+?>
+<div class="taglink"><a href="/categories/<?php echo urlrenamer($row['tag']).".shtml"; ?>" target="_blank" title="<?php echo ucwords($row['tag']); ?> Tubes"><?php echo ucwords($row['tag']); ?></a></div>
+<?php
+}
+?>
+</textarea>
